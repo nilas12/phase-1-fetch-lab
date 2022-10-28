f8b02ca29bedb2bbd954544c8260baa4788e9ba6
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-29 00:33:14 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(json));
+ 
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+function renderBooks(json) {
+  const main = document.querySelector("main");
+  json.forEach(book => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
     main.appendChild(h2);
   });
 }
-
+ 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
